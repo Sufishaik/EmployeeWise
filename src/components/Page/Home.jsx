b699e8943e84d9eaeccc6364fb8b8e4f3ef37346
sufiyan <shaikhsufiyan174@gmail.com>
2025-04-06 21:44:37 +0530

    updated

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -64,9 +64,11 @@ function Home() {
     }, []);
     return (
         <>
-            <div className='relative'>
-                <div className='overflow-hidden max-w-[85vw] mx-auto' >
-                    <div className='flex items-center justify-center absolute top-5 left-5  md:right-0 lg:right-0'>
+            <div className='relative overflow-hidden'>
+                <div className=' max-w-[85vw] mx-auto' >
+
+
+                    <div className='flex items-center justify-center absolute top-5 md:left-5 md:right-0 lg:right-0'>
 
                         <input
                             type="text"
@@ -81,56 +83,58 @@ function Home() {
                             Logout
                         </button>
                     </div>
-                    <table class="table" style={{ marginTop: "5rem" }}>
-                        <thead className=''>
-                            <tr className=''>
-                                <th scope="" className='w-[5vw]'>Sr.no</th>
-                                <th scope="" className='w-[20vw]'>Profile</th>
-                                <th scope="" className='w-[20vw]'>First</th>
-                                <th scope="" className='w-[20vw]'>Last</th>
-                                <th scope="" className='w-[20vw]'>Email</th>
-                                <th scope="" className='w-[20vw]'>Modify</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                userData && userData
-                                    ?.filter((user) => user.first_name.toLowerCase().includes(searchTerm.toLowerCase()))
-                                    ?.map?.((item, id) => {
-                                        return (
-                                            <>
-                                                <tr className='' key={id}>
-                                                    <th scope="row">{item?.id}</th>
-                                                    <td><img src={item?.avatar} className='w-[50px] h-[50px] rounded-[2rem]' alt="" /></td>
-                                                    <td>{item?.first_name}</td>
-                                                    <td>{item?.last_name}</td>
-                                                    <td>{item?.email}</td>
-                                                    <td>
-                                                        <div className='flex gap-2 cursor-pointer'>
-                                                            <Link to={`/edit/${item?.id}`}>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" onClick={() => {
+                    <div className='' style={{ overflow: "auto" }}>
+                        <table className="table " style={{ marginTop: "5rem" }}>
+                            <thead className=''>
+                                <tr className=''>
+                                    <th scope="" className='w-[5vw]'>Sr.no</th>
+                                    <th scope="" className='w-[20vw]'>Profile</th>
+                                    <th scope="" className='w-[20vw]'>First</th>
+                                    <th scope="" className='w-[20vw]'>Last</th>
+                                    <th scope="" className='w-[20vw]'>Email</th>
+                                    <th scope="" className='w-[20vw]'>Modify</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    userData && userData
+                                        ?.filter((user) => user.first_name.toLowerCase().includes(searchTerm.toLowerCase()))
+                                        ?.map?.((item, id) => {
+                                            return (
+                                                <>
+                                                    <tr className='' key={id}>
+                                                        <th scope="row">{item?.id}</th>
+                                                        <td><img src={item?.avatar} className='w-[50px] h-[50px] rounded-[2rem]' alt="" /></td>
+                                                        <td>{item?.first_name}</td>
+                                                        <td>{item?.last_name}</td>
+                                                        <td>{item?.email}</td>
+                                                        <td>
+                                                            <div className='flex gap-2 cursor-pointer'>
+                                                                <Link to={`/edit/${item?.id}`}>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={() => {
 
-                                                                }} width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                                                                    }} width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                                                                    </svg>
+                                                                </Link>
+                                                                <svg onClick={() => {
+                                                                    handleDelete(item?.id)
+                                                                }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                                                                 </svg>
-                                                            </Link>
-                                                            <svg onClick={() => {
-                                                                handleDelete(item?.id)
-                                                            }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                                                            </svg>
 
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            </>
-                                        )
-                                    })
-                            }
-                        </tbody>
-                    </table>
-                    <div className="flex justify-center items-center mt-6">
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                </>
+                                            )
+                                        })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="flex justify-center items-center mt-6 ">
                         <button
                             onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
                             disabled={currentPage === 1}
@@ -138,7 +142,7 @@ function Home() {
                         >
                             Previous
                         </button>
-                        <span>Page {currentPage} of {totalPages}</span>
+                        <span className='hidden md:flex lg:flex sm:flex '>Page {currentPage} of {totalPages}</span>
                         <button
                             onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
                             disabled={currentPage === totalPages}
